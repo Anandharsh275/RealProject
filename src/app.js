27195d742a00981c5295f71005e86b67b2fe4871
Anandharsh275 <anandharsh275@gmail.com>
2023-12-18 09:16:33 +0530

    just simple changes

--- src/app.js
+++ src/app.js
@@ -1,2 +1,9 @@
 const express=require('express');
 const a=express();
+const port = process.env.PORT || 800;
+app.get("", (req, res) => {
+    res.render("index")
+ });
+ app.listen(port,()=>{
+    console.log(`listening to the port http://localhost:${port}`)
+})
