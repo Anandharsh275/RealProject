52f94c403fd36fe3e7b1044e9acdea7f24932f83
Anandharsh275 <anandharsh275@gmail.com>
2023-12-18 11:03:21 +0530

    just add few things

--- src/app.js
+++ src/app.js
@@ -1,8 +1,18 @@
 const express=require('express');
-const a=express();
+const path=require('path');
+const app=express();
 const port = process.env.PORT || 800;
+const static_path = path.join(__dirname,"../public");
+
+app.use(express.static(static_path));//mainly used to give allowence for static file like html
 app.get("", (req, res) => {
     res.render("index")
+ });
+ app.get("/student", (req, res) => {
+    res.send("Student")
+ });
+ app.get("/Submit",(req, res) => {
+    res.send("Submit")
  });
  app.listen(port,()=>{
     console.log(`listening to the port http://localhost:${port}`)
