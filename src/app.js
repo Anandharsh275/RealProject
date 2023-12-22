f0482edf9a385151b47590f1882f7d3a8d94eeca
Anandharsh275 <anandharsh275@gmail.com>
2023-12-22 16:49:37 +0530

    just using handlebars

--- src/app.js
+++ src/app.js
@@ -3,20 +3,23 @@ const path=require('path');
 const app=express();
 const port = process.env.PORT || 800;
 const static_path = path.join(__dirname,"../public");
-
+app.set('view engine', 'hbs');
 app.use(express.static(static_path));//mainly used to give allowence for static file like html
 app.get("", (req, res) => {
-    res.render("index")
+    res.render("index.hbs")
  });
-//  app.get("/student", (req, res) => {
-//     res.send("Student")
-//  });
-//  app.get("/teacher", (req, res) => {
-//    res.send("teacher")
-// });
-//  app.get("/Submit",(req, res) => {
-//     res.send("Submit")
-//  });
+ app.get("/primary", (req, res) => {
+    res.render("primary.hbs")
+ });
+ app.get("/Secondary", (req, res) => {
+   res.render("Secondary")
+});
+ app.get("/Submit",(req, res) => {
+    res.send("Submit")
+ });
+ app.get("/login",(req,res)=>{
+    res.render("login");
+ })
  app.listen(port,()=>{
     console.log(`listening to the port http://localhost:${port}`)
 })
